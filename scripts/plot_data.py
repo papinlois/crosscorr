03cf3b77acb86c20baa48d29841a7e6cfba844c0
papinlois <papinlois@gmail.com>
2024-02-20 10:37:19 -0800

    Update plot_data.py

--- scripts/plot_data.py
+++ scripts/plot_data.py
@@ -7,16 +7,12 @@ Created on Fri Feb  2 13:10:18 2024
 """
 
 import os
-import random
+import csv
 from datetime import datetime, timedelta
-import warnings
-import numpy as np
-import pandas as pd
-from obspy import UTCDateTime
-import crosscorr_tools
+import matplotlib
 import matplotlib.pyplot as plt
-
-# warnings.filterwarnings("ignore", category=RuntimeWarning)
+import numpy as np
+import crosscorr_tools
 
 # Define the base directory
 base_dir = "/Users/lpapin/Documents/phd/"
@@ -80,12 +76,78 @@ data_plot_filename = os.path.join(
 )
 crosscorr_tools.plot_data(st, pairs, data_plot_filename)
 
-# ========== Plotting Stations ==========
+# ========== Plotting Stations and LFE Families of Bostock ==========
 
-# =============================================================================
-# ## TO UPDATE TO MORE STATIONS AND NETWORK
-# # Plot station locations
-# locfile = pd.read_csv('stations.csv')
-# locs = locfile[['Name', 'Longitude', 'Latitude','Network']].values
-# crosscorr_tools.plot_loc(locs, base_dir)
-# =============================================================================
+# Define the file path
+file_path = "stations.csv"
+
+# Create a larger figure
+plt.figure(figsize=(10, 8))
+
+# Open the file and read its contents
+with open(file_path, newline='') as csvfile:
+    # Create a CSV reader object
+    reader = csv.reader(csvfile, delimiter='|')
+    # Skip the header row
+    next(reader)
+    # Initialize empty lists to store data
+    latitudes = []
+    longitudes = []
+    networks = []
+    stations = []
+    # Iterate over each row in the CSV file
+    for row in reader:
+        # Extract the desired fields
+        network = row[0]
+        station = row[1]
+        latitude = float(row[2])
+        longitude = float(row[3])
+        # Append data to lists
+        networks.append(network)
+        stations.append(station)
+        latitudes.append(latitude)
+        longitudes.append(longitude)
+
+# Create a color map based on unique networks using the 'tab10' colormap
+unique_networks = list(set(networks))
+color_map = matplotlib.colormaps['tab10']
+
+# Plot each station with a triangle marker based on the network
+for i, network in enumerate(unique_networks):
+    network_indices = [j for j, net in enumerate(networks) if net == network]
+    network_latitudes = [latitudes[k] for k in network_indices]
+    network_longitudes = [longitudes[k] for k in network_indices]
+    plt.scatter(network_longitudes, network_latitudes, 
+                color=color_map(i), marker='^', s=150) #, label=network
+
+# Annotate each station with its name
+for i, (lon, lat, station) in enumerate(zip(longitudes, latitudes, stations)):
+    plt.text(lon, lat, station, fontsize=12, ha='left')
+
+
+# Load LFE family data
+sav_family_phases = np.load('./sav_family_phases.npy', allow_pickle=True).item()
+family_nb = np.array(list(sav_family_phases.keys()))
+# family_nb = family_nb[family_nb == '001']
+
+# Iterate through LFE families
+for nb in family_nb:
+    eqLoc_values = sav_family_phases[nb]['eqLoc']
+    lon_family, lat_family, z_family = eqLoc_values
+    lon_family = lon_family * -1
+    plt.scatter(lon_family, lat_family, color='grey', s=75)
+    plt.text(lon_family, lat_family, nb, fontsize=10, ha='left')
+
+# Set the limits of the plot
+plt.xlim(-124.5, -123)
+plt.ylim(48, 49.3)
+
+# Add labels and legend
+plt.xlabel('Longitude', fontsize=14)
+plt.ylabel('Latitude', fontsize=14)
+plt.title('Stations and LFE Families', fontsize=16)
+plt.gcf().subplots_adjust(bottom=0.2)
+plt.tight_layout()
+# plt.legend()
+plt.savefig('stations_lfe_families')
+plt.close()
